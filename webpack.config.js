bb4ef92948126b8e192a2c82db26aff1a67ea3bc
Eran Witkon <ewitkon@palantir.com>
2023-07-10 19:38:33 -0400

    fix packag-lock (again)

--- webpack.config.js
+++ webpack.config.js
@@ -28,9 +28,7 @@ module.exports = {
             ".mjs": [".mjs", ".mts"],
         },
         fallback: {
-            buffer: require.resolve("buffer"),
             querystring: require.resolve("querystring-es3"),
-            stream: require.resolve("stream-browserify"),
         },
     },
     plugins: [
