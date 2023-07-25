dce2cdee0a8bfce81117ae8e90264b34291e59ea
eran witkon <30193043+ewitkon@users.noreply.github.com>
2023-07-25 09:23:57 -0400

    fix code to use new results object (#17)
    Co-authored-by: Eran Witkon <ewitkon@palantir.com>

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,20 +1,36 @@
 import React, { useEffect, useState } from "react";
 import { client } from "../utils/client";
 import { Country } from "@ontology-starter/sdk/ontology/objects";
+import { ErrorVisitor, ListObjectsError, Result, visitError, isOk, isErr } from "@ontology-starter/sdk/";
 
 import "./home.scss";
 
 export const HomePage: React.FC = () => {
     const [objectList, setObjectList] = useState<
-        { status: "loading" } | { status: "loaded"; value: Country[] } | { status: "failed_loading" }
+        { status: "loading" } | { status: "loaded"; value: Country[] } | { status: "failed_loading"; msg: string }
     >({ status: "loading" });
+
+    const getData = React.useCallback(async () => {
+        const result: Result<Country[], ListObjectsError> = await client.ontology.objects.Country.all();
+        if (isOk(result)) {
+            setObjectList({ value: result.value, status: "loaded" });
+        } else if (isErr(result)) {
+            const visitor: ErrorVisitor<ListObjectsError, void> = {
+                ObjectTypeNotFound: err => {
+                    setObjectList({ status: "failed_loading", msg: `Object type ${err.objectType} was not found` });
+                },
+                default: () => {
+                    setObjectList({ status: "failed_loading", msg: "failed loading object type" });
+                },
+            };
+            visitError(result.error, visitor);
+        }
+    }, []);
 
     // Do an initial load of all Objects of a particular type
     useEffect(() => {
-        client.ontology.objects.Country.all().then((objects: Country[]) =>
-            setObjectList({ value: objects, status: "loaded" }),
-        );
-    }, []);
+        getData();
+    }, [getData]);
 
     return (
         <div className="home">
