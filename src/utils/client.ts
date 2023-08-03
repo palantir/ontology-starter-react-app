db76ff2a003ab94a41bf4eee9759bb1dc4a84b00
Eran Witkon <ewitkon@palantir.com>
2023-08-03 17:14:06 -0400

    update client auth

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -1,11 +1,14 @@
 // Replace the package name with your application's package name instead.
-import { FoundryBrowserClient } from "@ontology-starter/sdk";
+import { FoundryClient, PublicClientAuth } from "@ontology-starter/sdk";
 
 /**
  * Initialize the client to interact with the Ontology SDK
  */
-export const client = new FoundryBrowserClient({
-    clientId: process.env.FOUNDRY_CLIENT_ID!,
+export const client = new FoundryClient({
     url: process.env.FOUNDRY_API_URL!,
-    redirectUrl: process.env.APPLICATION_REDIRECT_URL!,
+    auth: new PublicClientAuth({
+        clientId: process.env.FOUNDRY_CLIENT_ID!,
+        url: process.env.FOUNDRY_API_URL!,
+        redirectUrl: process.env.APPLICATION_REDIRECT_URL!,
+    }),
 });
