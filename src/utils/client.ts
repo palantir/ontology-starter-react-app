88721dda2191fcfd6aa809f311834f8ce57df6ae
Eran Witkon <ewitkon@palantir.com>
2023-08-10 16:39:59 -0400

    Add develop route

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -1,14 +1,22 @@
 // Replace the package name with your application's package name instead.
-import { FoundryClient, PublicClientAuth } from "@ontology-starter/sdk";
+import { FoundryClient, PublicClientAuth, UserTokenAuth } from "@ontology-starter/sdk";
 
 /**
  * Initialize the client to interact with the Ontology SDK
  */
-export const client = new FoundryClient({
-    url: process.env.FOUNDRY_API_URL!,
-    auth: new PublicClientAuth({
-        clientId: process.env.FOUNDRY_CLIENT_ID!,
-        url: process.env.FOUNDRY_API_URL!,
-        redirectUrl: process.env.APPLICATION_REDIRECT_URL!,
-    }),
-});
+export let client: FoundryClient =
+    process.env.NODE_ENV === "production"
+        ? new FoundryClient({
+              url: process.env.FOUNDRY_API_URL!,
+              auth: new PublicClientAuth({
+                  clientId: process.env.FOUNDRY_CLIENT_ID!,
+                  url: process.env.FOUNDRY_API_URL!,
+                  redirectUrl: process.env.APPLICATION_REDIRECT_URL!,
+              }),
+          })
+        : new FoundryClient({
+              url: process.env.FOUNDRY_API_URL!,
+              auth: new UserTokenAuth({
+                  userToken: process.env.USER_TOKEN!,
+              }),
+          });
